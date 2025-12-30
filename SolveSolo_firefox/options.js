c649ccc80a99bbba8c4c53fd4204d7472352f8b6
oipi <tuberosumgames@gmail.com>
2025-12-31 00:36:09 +0530

    options revamp

--- SolveSolo_firefox/options.js
+++ SolveSolo_firefox/options.js
@@ -1,25 +1,45 @@
 //load saved settings
 document.addEventListener("DOMContentLoaded", async () => {
+    const durationInput = document.getElementById('duration');
+    const saveBtn = document.getElementById('save');
+    const statusMsg = document.getElementById('status');
+    
     chrome.storage.local.get(['blockDuration', 'blockYoutube'], (data) => {
         //default to 20 mins
         document.getElementById('duration').value = data.blockDuration || 20;
         //default to false
         document.getElementById('blockYoutube').checked = data.blockYoutube || false;
     });
-});
 
-//save settings
-document.getElementById('save').addEventListener('click', () => {
-    const duration = parseInt(document.getElementById('duration').value);
-    const blockYoutube = document.getElementById('blockYoutube').checked;
 
-    chrome.storage.local.set({
-        blockDuration: duration,
-        blockYoutube: blockYoutube
-    }, () => {
-        //show saved message for 1.5 seconds
-        const status = document.getElementById('status');
-        status.style.display = 'inline';
-        setTimeout(() => { status.style.display = 'none'; }, 1500);
+    //save button click handler
+    saveBtn.addEventListener('click', () => {
+        const duration = parseInt(durationInput.value);
+        const blockYoutube = document.getElementById('blockYoutube').checked;
+
+        //validation
+        if (isNaN(duration) || duration < 15) {
+            //error state
+            statusMsg.innerText = "Duration cannot be less than 15 minutes.";
+            statusMsg.className = "error"; 
+            statusMsg.style.display = "inline";
+            
+            //do not save
+            return; 
+        }
+
+        //save success
+        chrome.storage.local.set({
+            blockDuration: duration,
+            blockYoutube: blockYoutube
+        }, () => {
+            //success state
+            statusMsg.innerText = "Saved!";
+            statusMsg.className = "success";
+            statusMsg.style.display = "inline";
+            
+            //status message for 1.5 secs
+            setTimeout(() => { statusMsg.style.display = 'none'; }, 1500);
+        });
     });
 });
